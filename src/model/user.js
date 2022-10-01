d2dc1c4998eda6252a5cbdd2b2959c4c1ed945d2
haiphu27 <phunh@vgsholding.com>
2022-10-01 08:53:16 +0700

    fix load roouter + create table with sequelize

--- src/model/user.js
+++ src/model/user.js
@@ -1,22 +1,22 @@
 const {DataTypes} = require('sequelize')
-const {sequelize} = require('../config/connect-mysql')
 
-sequelize.define("users", {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
+module.exports = (sequelize) => {
+     sequelize.define("users", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
 
-});
-
-module.exports = sequelize.sync()
+    });
+     sequelize.sync()
+}
 
--- src/server.js
+++ src/server.js
@@ -3,30 +3,30 @@ const app = express()
 const cors = require("cors");
 require('dotenv').config()
 const {redisClient} = require('./config/connect-redis');
-const Load= require('./loadfile/load')
-const {sequelize}=require('./config/connect-mysql')
-const table_user=require('./model/user')
+const load_router = require('./loadfile/load-router')
+const {sequelize} = require('./config/connect-mysql')
+const table_user = require('./model/user')
 
-sequelize.authenticate().then(() => {
-   table_user
+sequelize.authenticate().then(async () => {
+   await table_user(sequelize)
     console.log('connected DB successfully')
 }).catch((error) => {
     console.error('Unable to connect to the database: ', error);
 });
 
 redisClient.connect()
-    .then(()=>{
+    .then(() => {
         console.log('connected to Redis server')
     })
-    .catch(err => console.error(err) )
+    .catch(err => console.error(err))
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-Load.load_all_router(app)
+load_router(app)
 
-const PORT=process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, function () {
     console.log("You are listening on http://localhost:" + PORT)
 })
